5dd6236560844d8085a26cacbf37788e95deb414
arturbazza <agencia@fonzie.com.br>
2024-04-15 21:44:33 -0300

    Báh!!! Os 3 retornos e os 3 no banco lçocal!!! :)

--- src/main/java/br/org/fundatec/atividade03/controller/AddressController.java
+++ src/main/java/br/org/fundatec/atividade03/controller/AddressController.java
@@ -3,15 +3,13 @@ package br.org.fundatec.atividade03.controller;
 import br.org.fundatec.atividade03.model.Address;
 import br.org.fundatec.atividade03.service.AddressService;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Getter
-@Setter
+import java.util.concurrent.CompletableFuture;
+
 @Data
 @RestController
 public class AddressController {
@@ -20,7 +18,7 @@ public class AddressController {
     private AddressService addressService;
 
     @GetMapping("/address/{cep}")
-    public Address getAddressByCep(@PathVariable String cep) {
+    public CompletableFuture<Address> getAddressByCep(@PathVariable String cep) {
         return addressService.findOrCreateByCep(cep);
     }
 }
--- src/main/java/br/org/fundatec/atividade03/service/AddressService.java
+++ src/main/java/br/org/fundatec/atividade03/service/AddressService.java
@@ -6,8 +6,6 @@ import br.org.fundatec.atividade03.controller.response.ViaCepResponse;
 import br.org.fundatec.atividade03.model.Address;
 import br.org.fundatec.atividade03.repository.AddressRepository;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -16,8 +14,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Getter
-@Setter
+import org.springframework.scheduling.annotation.Async;
+
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 @Data
 @Service
 public class AddressService {
@@ -25,42 +27,57 @@ public class AddressService {
     @Autowired
     private AddressRepository addressRepository;
 
-    public Address findOrCreateByCep(String cep) {
+    @Async
+    public CompletableFuture<Address> findOrCreateByCep(String cep) {
         Address existingAddress = addressRepository.findByCep(cep);
         if (existingAddress != null) {
-            return existingAddress;
+            return CompletableFuture.completedFuture(existingAddress);
         }
 
         RestTemplate restTemplate = new RestTemplate();
 
-        // Consulta no ViaCEP
-        String viaCepUrl = "https://viacep.com.br/ws/" + cep + "/json/";
-        ViaCepResponse viaCepResponse = restTemplate.getForObject(viaCepUrl, ViaCepResponse.class);
-        if (viaCepResponse != null) {
-            Address newAddress = createAddressFromResponse(viaCepResponse, "viacep");
-            return addressRepository.save(newAddress);
-        }
+        CompletableFuture<ViaCepResponse> viaCepFuture = CompletableFuture.supplyAsync(() -> {
+            String viaCepUrl = "https://viacep.com.br/ws/" + cep + "/json/";
+            return restTemplate.getForObject(viaCepUrl, ViaCepResponse.class);
+        });
 
-        // Consulta no BrasilAPI
-        String brasilApiUrl = "https://brasilapi.com.br/api/cep/v1/" + cep;
-        BrasilApiResponse brasilApiResponse = restTemplate.getForObject(brasilApiUrl, BrasilApiResponse.class);
-        if (brasilApiResponse != null) {
-            Address newAddress = createAddressFromResponse(brasilApiResponse, "brasilapi");
-            return addressRepository.save(newAddress);
-        }
+        CompletableFuture<BrasilApiResponse> brasilApiFuture = CompletableFuture.supplyAsync(() -> {
+            String brasilApiUrl = "https://brasilapi.com.br/api/cep/v1/" + cep;
+            return restTemplate.getForObject(brasilApiUrl, BrasilApiResponse.class);
+        });
 
-        // Consulta no CepAberto
-        String cepAbertoUrl = "https://www.cepaberto.com/api/v3/cep?cep=" + cep;
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Token token=21e77485e82bf19d0fba79125e8a75ff");
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-        ResponseEntity<CepAbertoResponse> response = restTemplate.exchange(cepAbertoUrl, HttpMethod.GET, entity, CepAbertoResponse.class);
-        CepAbertoResponse cepAbertoResponse = response.getBody();
-        if (cepAbertoResponse != null) {
-            Address newAddress = createAddressFromResponse(cepAbertoResponse, "cepaberto");
-            return addressRepository.save(newAddress);
-        }
-        return null;
+        CompletableFuture<CepAbertoResponse> cepAbertoFuture = CompletableFuture.supplyAsync(() -> {
+            String cepAbertoUrl = "https://www.cepaberto.com/api/v3/cep?cep=" + cep;
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Token token=21e77485e82bf19d0fba79125e8a75ff");
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+            ResponseEntity<CepAbertoResponse> response = restTemplate.exchange(cepAbertoUrl, HttpMethod.GET, entity, CepAbertoResponse.class);
+            return response.getBody();
+        });
+
+        CompletableFuture<Address> result = CompletableFuture.allOf(viaCepFuture, brasilApiFuture, cepAbertoFuture)
+                .thenApplyAsync(v -> {
+                    try {
+                        ViaCepResponse viaCepResponse = viaCepFuture.get();
+                        BrasilApiResponse brasilApiResponse = brasilApiFuture.get();
+                        CepAbertoResponse cepAbertoResponse = cepAbertoFuture.get();
+
+                        Address viaCepAddress = createAddressFromResponse(viaCepResponse, "viacep");
+                        Address brasilApiAddress = createAddressFromResponse(brasilApiResponse, "brasilapi");
+                        Address cepAbertoAddress = createAddressFromResponse(cepAbertoResponse, "cepaberto");
+
+                        addressRepository.save(viaCepAddress);
+                        addressRepository.save(brasilApiAddress);
+                        addressRepository.save(cepAbertoAddress);
+
+                        return viaCepAddress; // Pode retornar qualquer um dos endereços
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                });
+
+        return result;
     }
 
     private Address createAddressFromResponse(ViaCepResponse viaCepResponse, String site) {
